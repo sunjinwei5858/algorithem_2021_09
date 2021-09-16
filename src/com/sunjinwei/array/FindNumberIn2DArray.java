2b176b5735f6ae9aca3e4cc83ff19f2504e3b594
sunjinwei <sunjinwei5828@163.com>
2021-09-17 07:26:14 +0800

    feat(数组)：剑指 Offer 04. 二维数组中的查找

---
+++ src/com/sunjinwei/array/FindNumberIn2DArray.java
@@ -0,0 +1,49 @@
+package com.sunjinwei.array;
+
+/**
+ * 剑指 Offer 04. 二维数组中的查找
+ * <p>
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，
+ * 每一列都按照从上到下递增的顺序排序。
+ * 请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * ps:
+ * 1. 转化 想象成一颗bst 右上角为根节点 来进行二分
+ * 2. 注意鲁棒性的判断 i和j的范围
+ */
+public class FindNumberIn2DArray {
+
+    // 从左到右递增
+    // 从上到下递增
+    // 想到二分 把这个二维数组转移为二叉树 右上角为根节点
+    // 满足二叉搜索树的条件
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        // 鲁棒性
+        if (matrix == null) {
+            return false;
+        }
+        // 行
+        int m = matrix.length;
+        if (m == 0) {
+            return false;
+        }
+        // 列
+        int n = matrix[0].length;
+        if (n == 0) {
+            return false;
+        }
+        // 右上角的索引
+        int i = 0;
+        int j = n - 1;
+        // 开始进行比较
+        while (i >= 0 && j >= 0 && i < m && j < n) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
